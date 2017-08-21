fd754c7aba85094e8a026693963658f02ccd8f74
xix <krzysztof.wolf.1980@gmail.com>
2017-08-21 21:41:20 +0200

    remove obsolete projects

--- Glassfish-Oracle-Database-Events-DCN/src/main/resources/scripts/glassfish-setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#asadmin delete-jdbc-resource jdbc/cacheCordinationResource -p 9348
-#asadmin delete-jdbc-connection-pool cacheCordinationResourcePool -p 9348
-#asadmin create-jdbc-connection-pool --datasourceclassname oracle.jdbc.pool.OracleDataSource --restype javax.sql.DataSource --property url="jdbc\:oracle\:thin\:@localhost\:1521\:XE":user=cacheCordination:password=123 cacheCordinationResourcePool -p 9348
-#asadmin create-jdbc-resource --connectionpoolid=cacheCordinationResourcePool jdbc/cacheCordinationResource -p 9348
--- Glassfish-Second-Level-Cache-Coordination-RMI/src/main/resources/scripts/database-setup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sqlplus /nolog system/manager@jdbc:oracle:thin:@localhost:1521:XE @$1
